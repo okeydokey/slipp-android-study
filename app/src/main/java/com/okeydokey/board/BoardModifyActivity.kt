5ccda080a86790e66a56d20974d781fa5acf5f6e
okeydokey <yunkim28@gmail.com>
2018-08-05 02:11:20 +0900

    #4 게시글 등록 후 게시글 상세로 이동

--- app/src/main/java/com/okeydokey/board/BoardModifyActivity.kt
+++ app/src/main/java/com/okeydokey/board/BoardModifyActivity.kt
@@ -1,20 +1,21 @@
 package com.okeydokey.board
 
 import android.content.Intent
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
 
-class BoardRegisterActivity : AppCompatActivity() {
+class BoardModifyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_register)
+        setContentView(R.layout.activity_board_modify)
 
+        val no = intent.getLongExtra("board", 1)
         var category = findViewById<TextView>(R.id.category)
         val categoryNo = intent.getLongExtra("category", 1)
         category.text = categories[categoryNo.toInt() - 1].name
@@ -25,7 +26,6 @@ class BoardRegisterActivity : AppCompatActivity() {
             val title = findViewById<EditText>(R.id.title)
             val content = findViewById<EditText>(R.id.content)
 
-            var no = (boards.size + 1).toLong()
             val new = Board(no
                     , categories[categoryNo.toInt() - 1]
                     , title.text.toString()
@@ -34,7 +34,7 @@ class BoardRegisterActivity : AppCompatActivity() {
                     , SimpleDateFormat("yyyy.MM.dd").format(Date())
                     , "")
 
-            boards.add(new)
+            boards.add(no.toInt() - 1, new)
 
             intent = Intent(this, BoardDetailActivity::class.java)
             intent.putExtra("board", no)
